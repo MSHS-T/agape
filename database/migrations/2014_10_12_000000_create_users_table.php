5a0da37ae1b1cdf84518be199d25d2e1a77e0943
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-12-14 17:36:34 +0100

    Add phone file to user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('phone')->nullable();;
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedTinyInteger('role')->default(UserRole::Candidate);
