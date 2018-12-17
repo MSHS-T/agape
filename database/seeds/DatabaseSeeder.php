9ee5e505f3f096f97875d591248414521ac14187
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-12-17 13:26:38 +0100

    Add seeder for project calls

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(SettingsTableSeeder::class);
+        $this->call(ProjectCallsTableSeeder::class);
     }
 }
---
+++ database/seeds/ProjectCallsTableSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectCallsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('project_calls')->insert([
+            "type" => 1,
+            "year" => 2018,
+            "description" => "Description",
+            "application_start_date" => "2018-12-01",
+            "application_end_date" => "2018-12-31",
+            "evaluation_start_date" => "2019-01-01",
+            "evaluation_end_date" => "2019-01-31",
+            "number_of_experts" => 2,
+            "number_of_documents" => 4,
+            "privacy_clause" => "Clause",
+            "invite_email_fr" => "Email FR",
+            "invite_email_en" => "Email EN",
+            "help_experts" => "Aide experts",
+            "help_candidates" => "Aide candidats",
+            "closed" => 0,
+            "creator_id" => 1,
+            "created_at" => "2018-12-17 11:22:54",
+            "updated_at" => "2018-12-17 11:22:54"
+        ]);
+        DB::table('project_calls')->insert([
+            "type" => 2,
+            "year" => 2018,
+            "description" => "Description",
+            "application_start_date" => "2018-12-01",
+            "application_end_date" => "2018-12-31",
+            "evaluation_start_date" => "2019-01-01",
+            "evaluation_end_date" => "2019-01-31",
+            "number_of_experts" => 2,
+            "number_of_documents" => 4,
+            "privacy_clause" => "Clause",
+            "invite_email_fr" => "Email FR",
+            "invite_email_en" => "Email EN",
+            "help_experts" => "Aide experts",
+            "help_candidates" => "Aide candidats",
+            "closed" => 0,
+            "creator_id" => 1,
+            "created_at" => "2018-12-17 11:22:54",
+            "updated_at" => "2018-12-17 11:22:54"
+        ]);
+        DB::table('project_calls')->insert([
+            "type" => 3,
+            "year" => 2018,
+            "description" => "Description",
+            "application_start_date" => "2018-12-01",
+            "application_end_date" => "2018-12-31",
+            "evaluation_start_date" => "2019-01-01",
+            "evaluation_end_date" => "2019-01-31",
+            "number_of_experts" => 2,
+            "number_of_documents" => 4,
+            "privacy_clause" => "Clause",
+            "invite_email_fr" => "Email FR",
+            "invite_email_en" => "Email EN",
+            "help_experts" => "Aide experts",
+            "help_candidates" => "Aide candidats",
+            "closed" => 0,
+            "creator_id" => 1,
+            "created_at" => "2018-12-17 11:22:54",
+            "updated_at" => "2018-12-17 11:22:54"
+        ]);
+    }
+}
