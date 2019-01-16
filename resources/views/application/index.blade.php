894c7e1008c18d9f61ed377f69609821a3cd8071
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-01-16 16:22:34 +0100

    Fix datetime display

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -33,10 +33,10 @@
                 <td>{{ $application->id}}</td>
                 <td>{{ $application->applicant->name }}</td>
                 <td>
-                    {{ \Carbon\Carbon::parse($application->created_at)->format(__('locale.date_format'))}}
+                    {{ \Carbon\Carbon::parse($application->created_at)->format(__('locale.datetime_format'))}}
                 </td>
                 <td>
-                    {{ \Carbon\Carbon::parse($application->submitted_at)->format(__('locale.date_format'))}}
+                    {{ \Carbon\Carbon::parse($application->submitted_at)->format(__('locale.datetime_format'))}}
                 </td>
                 <td>
                     <a href="
@@ -49,60 +49,4 @@
         </tbody>
     </table>
 </div>
-
-<div class="modal fade" id="confirm-archive" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">{{ __('actions.confirm_archive.title') }}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="{{__('actions.close')}}">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>{{ __('actions.confirm_archive.body') }}</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('actions.cancel') }}</button>
-                <form id="confirmation-form" action="" method="post">
-                    @csrf @method('DELETE')
-                    <button class="btn btn-danger" type="submit">{{ __('actions.archive') }}</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
-
-@push('scripts')
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-        $('.archive-link').click(function (e) {
-            e.preventDefault();
-            var targetUrl = jQuery(this).attr('href');
-            $("form#confirmation-form").attr('action', targetUrl);
-            $(".modal#confirm-archive").modal();
-        });
-        $('#projectcall_list').DataTable({
-            autoWidth: false,
-            lengthChange: true,
-            searching: true,
-            ordering: true,
-            order: [
-                [6, 'desc']
-            ],
-            columns: [null, null, null, null, null, null, null, null, {
-                searchable: false
-            }],
-            language: @json(__('datatable')),
-            pageLength: 5,
-            lengthMenu: [
-                [5, 10, 25, 50, -1],
-                [5, 10, 25, 50, "@lang('datatable.all')"]
-            ]
-        });
-    });
-
-</script>
-@endpush
