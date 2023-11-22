5179db372c747b18edfeddd488697c485c4c5923
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-11-22 20:12:53 +0100

    Hide contact link in navigation if user is administrator or manager

--- resources/views/components/filament/contact-link.blade.php
+++ resources/views/components/filament/contact-link.blade.php
@@ -1,4 +1,8 @@
-<x-filament::link class="flex justify-center items-center p-2 rounded-full border border-white"
-    href="{{ route('contact') }}" :tooltip="__('pages.contact.title')" target="_blank">
-    <x-fas-message class="h-4 w-4" />
-</x-filament::link>
+@auth
+    @hasanyrole(['applicant', 'expert'])
+        <x-filament::link class="flex justify-center items-center p-2 rounded-full border border-white"
+            href="{{ route('contact') }}" :tooltip="__('pages.contact.title')" target="_blank">
+            <x-fas-message class="h-4 w-4" />
+        </x-filament::link>
+    @endhasanyrole
+@endauth
