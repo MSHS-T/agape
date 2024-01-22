dd3ae032bcbe0be2a202c17e86b5f63893972797
Romain GONÇALVES <romain.goncalves@gmail.com>
2024-01-22 11:06:43 +0100

    Fixes languages switcher on login page

--- resources/views/components/language-switcher.blade.php
+++ resources/views/components/language-switcher.blade.php
@@ -1,5 +1,5 @@
 @php
-    $localeData = config('filament-language-switch.locales');
+    $flags = config('agape.flags');
     $locale = app()->getLocale();
     $allLocales = config('agape.languages');
 @endphp
@@ -7,10 +7,10 @@
 <button type="button" x-data="language_switcher" x-on:click="next"
     {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out']) }}>
     <div class="w-full flex items-center justify-start">
-        <x-dynamic-component :component="'flag-1x1-' . (!blank($localeData[$locale]['flag_code']) ? $localeData[$locale]['flag_code'] : 'un')"
+        <x-dynamic-component :component="'flag-1x1-' . ($flags[$locale] ?? $locale)"
             class="flex-shrink-0 w-4 h-4 mr-2 group-hover:text-white group-focus:text-white text-primary-500"
             style="border-radius: 0.25rem" />
-        {{ $localeData[$locale]['native'] }}
+        {{ str(locale_get_display_name($locale, app()->getLocale()))->title()->toString() }}
     </div>
 </button>
 @push('scripts')
