182b65298660b598c1f312b30cb7d232ebf05624
Romain GONÇALVES <romain.goncalves@gmail.com>
2024-02-07 16:06:45 +0100

    Adds test case for bug with application reference generation

--- tests/Unit/ApplicationReferenceTest.php
+++ tests/Unit/ApplicationReferenceTest.php
@@ -49,4 +49,37 @@ class ApplicationReferenceTest extends TestCase
 
         $this->assertEquals($application->reference, $projectCall->reference . '-003');
     }
+    /**
+     * A basic test example.
+     */
+    public function test_that_application_references_are_generated_properly_when_call_reference_contains_dashes(): void
+    {
+        $projectCallType = ProjectCallType::factory([
+            'reference' => 'AAA-BBB-CCC-DDD'
+        ])->create();
+        $projectCall = ProjectCall::factory([
+            'project_call_type_id' => $projectCallType->id
+        ])->application()
+            ->create();
+
+        $application = $projectCall->applications()->create([
+            'title' => fake()->sentence(4)
+        ]);
+
+        ray($application->reference);
+
+        $this->assertEquals($application->reference, $projectCall->reference . '-001');
+
+        $application = $projectCall->applications()->create([
+            'title' => fake()->sentence(4)
+        ]);
+
+        $this->assertEquals($application->reference, $projectCall->reference . '-002');
+
+        $application = $projectCall->applications()->create([
+            'title' => fake()->sentence(4)
+        ]);
+
+        $this->assertEquals($application->reference, $projectCall->reference . '-003');
+    }
 }
