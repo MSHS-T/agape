9c9c7ceb035c2c05a18e41bb2e2e755a6dd939e1
Romain GONÇALVES <romain.goncalves@gmail.com>
2024-07-07 13:15:53 +0200

    Removes obsolete middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,11 +62,9 @@ class Kernel extends HttpKernel
         'guest'              => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm'   => \Illuminate\Auth\Middleware\RequirePassword::class,
         'precognitive'       => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
-        'redirect_admins'    => \App\Http\Middleware\RedirectAdminsToFilament::class,
         'signed'             => \App\Http\Middleware\ValidateSignature::class,
         'throttle'           => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'           => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
         'role'               => \Spatie\Permission\Middleware\RoleMiddleware::class,
         'permission'         => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
