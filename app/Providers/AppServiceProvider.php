a6f441c40de15a84b0033e31ad9426a011aa172f
Romain GONÇALVES <romain.goncalves@gmail.com>
2024-10-24 18:27:46 +0200

    Adds sentry user details

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,18 +27,29 @@ class AppServiceProvider extends ServiceProvider
         }
         FilamentView::registerRenderHook(
             'panels::user-menu.before',
-            fn () => view('components.filament.contact-link')
+            fn() => view('components.filament.contact-link')
         );
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $languages = config('agape.languages');
             $flagOverrides = config('agape.flags');
-            $flags = collect($languages)->mapWithKeys(fn ($lang) => [
+            $flags = collect($languages)->mapWithKeys(fn($lang) => [
                 $lang => asset('vendor/blade-country-flags/1x1-' . ($flagOverrides[$lang] ?? $lang) . '.svg')
             ])->all();
             $switch->locales($languages)
                 ->renderHook('panels::user-menu.before')
                 ->flags($flags)
                 ->circular();
+        });
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope): void {
+            $user = auth()->user();
+            if ($user) {
+                $scope->setUser([
+                    'email' => $user->email,
+                    'id'    => $user->id,
+                    'role'  => $user->getRoleNames()->implode(','),
+                    'name'  => $user->name,
+                ]);
+            }
         });
     }
 }
